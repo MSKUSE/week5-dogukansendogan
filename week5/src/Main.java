b009079b01875eaefd6c6fe780cb7e9326361ce0
dogukansendogan <dogukansendogan@posta.mu.edu.tr>
2025-03-20 13:19:01 +0300

    dogukansendogan

--- week5/src/Main.java
+++ week5/src/Main.java
@@ -1,5 +1,31 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+public class Circle {
+
+    private Point center;
+    private int radius;
+
+    public Point getCenter() {
+        return center;
+    }
+
+    public void setCenter(Point center) {
+        this.center = center;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        if(radius<0){
+            this.radius=0;
+            System.out.println("radius cant be 0");
+            else{
+                this.radius=radius;
+            }
+        }
+
+
+        this.radius = radius;
     }
 }
